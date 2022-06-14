974e153b71203e8337ec8bda6476451f586e8eaf
Karlo <karlo@karloprikratki.com>
2022-06-14 11:31:36 +0200

    Initial commit 2

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="raiderio-async",
+    version="0.62.4",
+    packages=["raiderio_async"],
+    author="karlsbjorn",
+    install_requires=["aiohttp"],
+)
