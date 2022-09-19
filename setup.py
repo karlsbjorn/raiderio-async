61c9493c295f0a382b5d8b5dcad7629786b15eec
Karlo <karlo@karloprikratki.com>
2022-09-19 23:57:30 +0200

    allow python 3.8

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="raiderio-async",
-    version="1.1.0",
+    version="1.1.1",
     packages=["raiderio_async"],
     author="karlsbjorn",
     install_requires=["aiohttp", "aiolimiter"],
-    python_requires=">=3.9",
+    python_requires=">=3.8",
 )
