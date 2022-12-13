05dbbb8140cf4fc8966e9d8178931337f7397a50
Karlo <karlo@karloprikratki.com>
2022-12-13 21:59:03 +0100

    add caching

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="raiderio-async",
-    version="1.1.1",
+    version="1.2.0",
     packages=["raiderio_async"],
     author="karlsbjorn",
-    install_requires=["aiohttp", "aiolimiter"],
+    install_requires=["aiohttp-client-cache==0.7.3", "aiosqlite~=0.17.0", "aiolimiter~=1.0.0"],
     python_requires=">=3.8",
 )
